fa5f54ba42f11b2a813c9e102bedcc1430dfac79
phamducbinhh <phamducbinh1712000@gmail.com>
2026-07-22 19:53:38 +0700

    Refactor Vite configuration to load environment variables and improve production setup

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import frappeui from 'frappe-ui/vite';
@@ -9,56 +9,86 @@ import Icons from 'unplugin-icons/vite';
 import IconsResolver from 'unplugin-icons/resolver';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 
-export default defineConfig({
-	plugins: [
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	const isProduction = mode === 'production';
+
+	const plugins = [
 		vue(),
 		vueJsx(),
 		pluginRewriteAll(),
 		frappeui(),
+
 		Components({
 			dirs: [
 				'src/components',
-				// 'src2/components',
 				'node_modules/frappe-ui/src/components'
 			],
 			resolvers: [IconsResolver()]
 		}),
-		Icons(),
-		sentryVitePlugin({
-			url: process.env.SENTRY_URL,
-			org: process.env.SENTRY_ORG,
-			project: process.env.SENTRY_PROJECT,
-			applicationKey: 'press-dashboard',
-			authToken: process.env.SENTRY_AUTH_TOKEN
-		})
-	],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src')
-		}
-	},
-	optimizeDeps: {
-		include: ['feather-icons', 'showdown']
-	},
-	build: {
-		outDir: '../press/public/dashboard',
-		emptyOutDir: true,
-		sourcemap: true,
-		target: 'es2015',
-		rollupOptions: {
-			input: {
-				main: path.resolve(__dirname, 'index.html')
+
+		Icons()
+	];
+
+	if (
+		isProduction &&
+		env.SENTRY_URL &&
+		env.SENTRY_ORG &&
+		env.SENTRY_PROJECT &&
+		env.SENTRY_AUTH_TOKEN
+	) {
+		plugins.push(
+			sentryVitePlugin({
+				url: env.SENTRY_URL,
+				org: env.SENTRY_ORG,
+				project: env.SENTRY_PROJECT,
+				applicationKey: 'press-dashboard',
+				authToken: env.SENTRY_AUTH_TOKEN
+			})
+		);
+	}
+
+	return {
+		plugins,
+
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, 'src')
+			}
+		},
+
+		optimizeDeps: {
+			include: ['feather-icons', 'showdown']
+		},
+
+		esbuild: {
+			drop: isProduction ? ['console', 'debugger'] : []
+		},
+
+		build: {
+			outDir: '../press/public/dashboard',
+			emptyOutDir: true,
+
+			sourcemap: false,
+
+			target: 'es2015',
+
+			rollupOptions: {
+				input: {
+					main: path.resolve(__dirname, 'index.html')
+				}
+			}
+		},
+
+		// @ts-ignore
+		test: {
+			globals: true,
+			environment: 'jsdom',
+			setupFiles: 'src/tests/setup/msw.js',
+			coverage: {
+				extension: ['.vue', '.js'],
+				all: true
 			}
 		}
-	},
-	// @ts-ignore
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: 'src/tests/setup/msw.js',
-		coverage: {
-			extension: ['.vue', '.js'],
-			all: true
-		}
-	}
+	};
 });
